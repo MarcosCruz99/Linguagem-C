c8aeece4dd2fc81faf1e0006195dfc5dd9c0beed
Marcos Paulo <marcospauloocruz99@gmail.com>
2025-08-13 21:41:30 -0300

    Add files via upload

---
+++ aula130825.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+/*
+{
+double fabrica, dist, imposto, custo;
+printf("Informe o custo da fabrica:");
+scanf("%lf", &fabrica);
+dist = fabrica * 0.28;
+imposto = fabrica * 0.45;
+custo = fabrica + dist + imposto;
+printf("O custo total eh: %.2lf\n", custo);
+return 0;
+}
+*/
+
+/*
+{
+    int num, dezena, aux;
+    printf("Informe um numero de 3 digitos:");
+    scanf("%d", &num);
+    aux = num/10;
+    dezena = aux % 10;
+    printf("Dezena: %d\n", dezena);
+    return 0;
+}
+*/
+/*
+ {
+    int num, dia, mes, ano, aux;
+    printf("Informe o numero no formato ddmmaa:");
+    scanf("%d", &num);
+    dia = num/1000000;
+    aux = num % 1000000;
+    mes = aux / 10000;
+    ano = aux % 10000;
+    printf("Dia: %d Mes: %d Ano: %d", dia, mes, ano);
+    return 0;
+ }
+*/
+/*
+{
+    double angulo, seno, cosseno, tangente;
+    printf("Informe o valor do angulo:");
+    scanf("%lf", &angulo);
+    seno = sin(angulo);
+    cosseno  = cos(angulo);
+    tangente = tan(angulo);
+    printf("Seno: %.2lf\n", seno);
+    printf("Cosseno: %.2lf\n", tangente);
+    return 0;
+}
+*/
+/*
+{
+    double num, raiz;
+    printf("Informe um numero:");
+    scanf("%lf", &num);
+    raiz = sqrt (num);
+    printf("A raiz do numero eh: %.2lf\n", raiz);
+    return 0;
+}
+*/
+/*
+{
+    double x, y, resultado;
+    printf("Informe o valor de x:");
+    scanf("%lf", &x);
+    printf("Informe o valor de y:");
+    scanf("%lf", &y);
+    resultado = pow(x, y);
+    printf("Resultado: %.2lf\n", resultado);
+    return 0;
+}
+*/
+/*
+{
+    double num;
+    printf("Informe um numero:");
+    scanf("%lf", &num);
+    printf("Floor: %lf\n", floor(num));
+    printf("Ceil: %lf\n", ceil(num));
+    printf("Round: %lf\n", round(num));
+    return 0;
+}
+*/
+/*
+{
+    double num, quad, cubo;
+    printf("Informe um numero:");
+    scanf("%lf", &num);
+    quad = pow(num, 2);
+    cubo - pow(num, 3);
+    printf("Quadrado: %.2lf\nCubo: %.2lf", quad, cubo);
+    return 0;
+}
+*/
+{
+    double x, y, resultado;
+    printf("Informe um numero:");
+    scanf("%lf", &x);
+    printf("Informe o segundo numero:");
+    scanf("%lf", &y);
+    resultado = sqrt (x*y);
+    printf("Resultado: %.2lf\n", resultado);
+    return 0;
+}
