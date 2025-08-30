ade17dfc8ad7ca79ece1d3a6540f7c48298b75ae
Marcos Paulo <marcospauloocruz99@gmail.com>
2025-08-29 21:47:43 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,99 +1,119 @@
 #include <stdio.h>
 /*
-main ()
-
+int main()
 {
-    int n1, n2, soma;
-    printf("Informe o primeiro n�mero:\n");
-    scanf("%d", &n1);
-    printf("Informe o segundo numero:");
-    scanf("%d", &n2);
-    soma = n1 + n2;
-    if(soma > 10) {
-        printf("A soma �: %d", soma);
+    double n1, n2;
+    char operacao;
+    printf("Informe dois numero reais:");
+    scanf("%lf %lf", &n1, &n2);
+    printf("Informe a operacao: +, -, * ou /");
+    scanf(" %c", &operacao);
+    switch(operacao){
+    case '+':{
+    printf("Soma: %.lf\n", n1 + n2);
+    break;
+    }
+    case '-':{
+    printf("Subtracao: %.2lf\n", n1 - n2);
+    break;
+    }
+    case '*':{
+    printf("Multiplicacao: %.lf\n", n1 * n2);
+    break;
+    }
+    case '/':{
+    if(n2 == 0){
+        printf("Divisao por zero!\n");
     }
     else{
-        printf("Soma menor que 10");
+        printf("Divisao: %.2lf", n1/n2);
+        }
+    break;
+    }
+    default:{
+    printf("Operacao invalida!\n");
+    }
     }
     return 0;
 }
 */
 /*
+ int main()
+ {
+     int n = 1;
+     while(n <= 10000){
+        printf("%d\n", n);
+        n++;
+     }
+ return 0;
+ }
+*/
+/*
+int main ()
 {
-    int n;
-    printf("Informe um numero:\n");
-    scanf("%d", &n);
-    if(n > 0){
-        printf("Numero positivo!");
+    int num, fat = 1;
+    printf("Informe um numero:");
+    scanf("%d", &num);
+    while(num >= 1){
+        fat *= num;
+        num --;
     }
-    else if(n == 0){
-        printf("Numero nulo!");
-    }
-    else{
-        printf("Numero negativo!");
-    }
+    printf("Fatorial: %d", fat);
     return 0;
 }
 */
 /*
+int main()
 {
-    int n1, n2;
-    printf("Informe o primeiro numero:");
-    scanf("%d", &n1);
-    printf("Informe o segundo numero:");
-    scanf("%d", &n2);
-    if(n1 % 2== 0 && n2 % 2 == 0){
-        printf("Os dois numero sao pares!");
+    int cont = 1, num, soma = 0;
+    while(cont <= 10){
+        printf("Informe o %d ° numero:", cont);
+        scanf("%d", &num);
+        soma += num;
+        cont++;
     }
-    else if(n1 % 2 == 0 || n2 % 2 == 0){
-        printf("Um dos dois numero eh par!");
-    }
-    else{
-        printf("Os dois numeros sao impares!");
-    }
+    printf("A soma dos numero eh: %d.", soma);
     return 0;
 }
 */
 /*
+int main ()
 {
-    int n1, n2;
-    printf("Informe o primeiro numero:");
-    scanf("%d", &n1);
-    printf("Informe o segundo numero:");
-    scanf("%d", &n2);
-    if(n1 % 2== 0 && n2 % 2 == 0){
-            printf("Os dois numero sao pares!");
+    int cont = 1, num, somaP = 0, neg = 0;
+    while(cont <= 15){
+        printf("Informe o %d ° numero:", cont);
+        scanf("%d", &num);
+        if(num >= 0){
+            somaP = somaP + num; //somaP += num;
+        }
+        else{
+            neg++; //neg = neg + 1;
+        }
+        cont++;
     }
-    else if(n1 % 2== 0){
-        printf("O primeiro numero eh par!");
-    }
-    else if(n2 % 2 == 0){
-        printf("O segundo numero eh par!");
-    }
-    else{
-        printf("Os dois numeros sao impares!");
-    }
+    printf("Soma dos positivos: %d\n", somaP);
+    printf("Quantidade de negativos: %d\n", neg);
     return 0;
 }
 */
 int main ()
 {
-    char sexo;
-    double altura, peso;
-    printf("Informe o sexo:");
-    scanf("%c", &sexo);
-    printf("Informe a altura:");
-    scanf("%lf", &altura);
-    if(sexo == 'M' || sexo == 'm'){
-        peso = (72.7 * altura) - 58;
-        printf ("Peso ideal: %.2lf\n", peso);
+    int num, maior, menor, cont = 2;
+    printf("Informe o 1° numero:");
+    scanf("%d", &num);
+    maior = num;
+    menor = num;
+    while(cont <= 10){
+        printf("Informe o %d° numero:", cont);
+        scanf("%d", &num);
+        if(num > maior){
+            maior = num;
+        }
+        else if(num < menor){
+           menor = num;
+        }
+        cont++;
     }
-    else if(sexo == 'f' || sexo == 'F'){
-        peso = (62.1 * altura) - 44.7;
-        printf("Peso ideal: %.2lf\n", peso);
-    }
-    else{
-        printf("Opcao invalida!");
-    }
+    printf("Maior: %d\nMenor: %d\n", maior, menor);
     return 0;
 }
